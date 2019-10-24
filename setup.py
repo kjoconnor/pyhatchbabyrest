7804413a9270329b4cca205739f0930a9cbea18b
Kevin O'Connor <kevin@reddit.com>
2019-10-24 14:19:55 -0700

    Bump version to 1.0.1.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyhatchbabyrest",
-    version="1.0.0",
+    version="1.0.1",
     author="Kevin O'Connor",
     author_email="kjoconnor@gmail.com",
     description="Python library to control Hatch Baby Rest devices",
